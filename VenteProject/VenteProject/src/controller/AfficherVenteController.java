e271c9fddff5909d3db5f3bf49eeba89fec628cd
Anas-Basta <126124334+Anas-Basta@users.noreply.github.com>
2023-05-03 14:18:37 +0100

    somme des prix totale pdf

--- VenteProject/VenteProject/src/controller/AfficherVenteController.java
+++ VenteProject/VenteProject/src/controller/AfficherVenteController.java
@@ -8,6 +8,7 @@ package controller;
 
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Element;
 import com.itextpdf.text.Paragraph;
 import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfWriter;
@@ -188,26 +189,41 @@ private void Pdf(ActionEvent event) throws FileNotFoundException, DocumentExcept
     lisre = pv.getAllVente();
 
     // Define table with 5 columns
-    PdfPTable table = new PdfPTable(4);
+PdfPTable table = new PdfPTable(4);
+table.setWidthPercentage(100); // définir la largeur de la table à 100% de la page
 
-    // Add table headers
-    table.addCell("Client");
-    table.addCell("Produit");
-    table.addCell("Prix Total");
-    table.addCell("Date");
+// Add table headers
+table.addCell("Client");
+table.addCell("Produit");
+table.addCell("Prix Total");
+table.addCell("Date");
 
-    // Add data to table
-    for (Vente v : lisre){
-        table.addCell(v.getClient());
-        table.addCell(v.getProduit());
-        table.addCell(String.valueOf(v.getPrix_totale()));
-        table.addCell(String.valueOf(v.getDate()));
-    }
+// Add data to table
+for (Vente v : lisre) {
+    table.addCell(v.getClient());
+    table.addCell(v.getProduit());
+    table.addCell(String.valueOf(v.getPrix_totale()));
+    table.addCell(String.valueOf(v.getDate()));
+}
 
-    // Add table to document
-    document.add(table);
+// Calculate total sum of prices
+double total = 0.0;
+for (Vente v : lisre) {
+    total += v.getPrix_totale();
+}
 
-    document.close();   
+// Add table to document
+document.add(table);
+
+// Add total sum of prices
+Paragraph paragraph = new Paragraph("Somme des Totales: " + total);
+paragraph.setAlignment(Element.ALIGN_RIGHT); // aligner le texte à droite
+paragraph.setSpacingBefore(10f); // ajouter 10 unités d'espace avant le texte
+paragraph.setIndentationLeft(50f); // ajouter une indentation gauche de 50 unités
+document.add(paragraph);
+
+document.close();
+   
  }
 
 }
